25d9beca0835acab47110cf60ae0f51c36e8b83b
Hamzaabro5 <hamzaabro683@gmail.com>
2024-09-09 18:00:49 +0500

    React Quiz App

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import axios from 'axios';
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
-function App() {
-  const [question , setQuestion] = useState(``)
- 
+const App = () => {
+    let [quiz, setQuiz] = useState([])
+    const [index, setIndex] = useState(0)
 
-   useEffect(() => {
-     
-    axios("https://the-trivia-api.com/v2/questions")
-    .then((res) => {
-      console.log(res.data)
-      setQuestion(res.data)
-    }).catch((err) => {
-      console.log(err);
-    })
+    useEffect(() => {
+        async function getData() {
+            let questions = await axios('https://the-trivia-api.com/v2/questions')
+            console.log(questions.data);
+            setQuiz(questions.data)
+        }
+        getData()
+    }, [])
 
-   
-   }, [])
-   
-  return (
-    <>
+    function nextQuestion() {
+        index < quiz.length - 1 ? setIndex(index + 1) : alert('question khatum ho gain hain')
+    }
 
-    </>
-  )
+
+    return (
+        <>
+            <h1>Quiz App</h1>
+            <div>
+                {quiz.length > 0 ? <h1>Q{index + 1}: {quiz[index].question.text}</h1> : <h1>Loading...</h1>}
+            </div>
+            <button onClick={nextQuestion}>Next</button>
+        </>
+    )
 }
 
 export default App
-
-
-
-
-
-
